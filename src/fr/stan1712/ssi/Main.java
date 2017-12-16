73cdafdfca78d1aeee789e8fe5fa7ca2e6965a68
stanislasbdx <perso@stan1712.fr>
2017-12-16 15:01:43 +0100

    Rebuild

---
+++ src/fr/stan1712/ssi/Main.java
@@ -0,0 +1,22 @@
+package fr.stan1712.ssi;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener{
+	  public void onEnable()
+	  {
+	    saveDefaultConfig();
+	    
+	    System.out.println("[SSI] SmoothServerInfo enabled");
+	    
+	    PluginManager pm = getServer().getPluginManager();
+	    pm.registerEvents(new commands(this), this);
+	  }
+	  
+	  public void onDisable()
+	  {
+	    System.out.println("[SSI] SmoothServerInfo disabled");
+	  }	  
+}
---
+++ src/fr/stan1712/ssi/commands.java
@@ -0,0 +1,81 @@
+package fr.stan1712.ssi;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class commands implements Listener {
+	
+	private Main main;
+	
+	public commands(Main smoothinfo)
+	{
+	  this.main = smoothinfo;
+	}
+
+	@EventHandler
+	public void onCommandes(PlayerCommandPreprocessEvent e){
+	  Player p = e.getPlayer();
+	  String msg = e.getMessage();
+	  String[] args = msg.split(" ");
+  
+		if (args[0].equalsIgnoreCase("/ssi")){
+			if (p.hasPermission("smoothinfo.main")){
+				p.sendMessage(ChatColor.YELLOW + "SmoothServerInfo " + ChatColor.BLUE + "v1.0");
+				p.sendMessage(ChatColor.BLUE + "/iwebsite");
+				p.sendMessage(ChatColor.BLUE + "/idiscord");
+				p.sendMessage(ChatColor.BLUE + "/iforum");
+			}
+			else{
+				p.sendMessage(this.main.getConfig().getString("messages.no-permission").replace("&", "§"));
+			}
+			e.setCancelled(true);
+		}
+		  
+		if (args[0].equalsIgnoreCase("/iwebsite")){
+			if (p.hasPermission("smoothinfo.website")){
+				p.sendMessage(this.main.getConfig().getString("messages.show-website").replace("&", "§"));
+				p.sendMessage(this.main.getConfig().getString("social.website").replace("&", "§"));
+			}
+			else{
+				p.sendMessage(this.main.getConfig().getString("messages.no-permission").replace("&", "§"));
+			}
+			e.setCancelled(true);
+		}
+		  
+		if (args[0].equalsIgnoreCase("/idiscord")){
+			if (p.hasPermission("smoothinfo.discord")){
+				p.sendMessage(this.main.getConfig().getString("messages.show-discord").replace("&", "§"));
+				p.sendMessage(this.main.getConfig().getString("social.discord").replace("&", "§"));
+			}
+			else{
+				p.sendMessage(this.main.getConfig().getString("messages.no-permission").replace("&", "§"));
+			}
+			e.setCancelled(true);
+		}
+	  
+		if (args[0].equalsIgnoreCase("/iforum")){
+			if (p.hasPermission("smoothinfo.forum")){
+				p.sendMessage(this.main.getConfig().getString("messages.show-forum").replace("&", "§"));
+				p.sendMessage(this.main.getConfig().getString("social.forum").replace("&", "§"));
+			}
+			else{
+				p.sendMessage(this.main.getConfig().getString("messages.no-permission").replace("&", "§"));
+			}
+			e.setCancelled(true);
+		}
+		
+		if (args[0].equalsIgnoreCase("/iteamspeak")){
+			if (p.hasPermission("smoothinfo.teamspeak")){
+				p.sendMessage(this.main.getConfig().getString("messages.show-teamspeak").replace("&", "§"));
+				p.sendMessage(this.main.getConfig().getString("social.teamspeak").replace("&", "§"));
+			}
+			else{
+				p.sendMessage(this.main.getConfig().getString("messages.no-permission").replace("&", "§"));
+			}
+			e.setCancelled(true);
+		}
+	}
+}
